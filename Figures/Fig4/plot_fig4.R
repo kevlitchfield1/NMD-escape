f0052d987cea703b1c9beb053218c05798230018
kevlitchfield1 <40664105+kevlitchfield1@users.noreply.github.com>
2020-09-04 09:41:26 +0100

    Update plot_fig4.R

--- Figures/Fig4/plot_fig4.R
+++ Figures/Fig4/plot_fig4.R
@@ -19,11 +19,15 @@ cbPalette2 <- c("darkred","darkblue")
 
 # ACT
 my_comparisons <- list( c("no_benefit","benefit"))
-dat<-read.table("NMD/act_nmd_data.txt",header=T)
+dat<-read.table("./act_nmd_data.txt",header=T)
 dat$response<-factor(dat$response,levels = c("no_benefit","benefit"),ordered = TRUE)
 ac_snv_ri<-ggplot(data=dat,aes(x=response, y=TMB,color=response))+geom_boxplot(outlier.shape = NA) +geom_quasirandom(size = 3, shape=18)+ theme(legend.position = "none",text = element_text(size=10),axis.text.x = element_text(size=10),axis.text.y = element_text(size=10))+stat_compare_means(comparisons = my_comparisons,)+scale_fill_manual(values=cbPalette2)+scale_color_manual(values = cbPalette2)+ theme(plot.margin = unit(c(1,1,1,1), "lines"))+ ggtitle("")+xlab("")
 ac_fs_ri<-ggplot(data=dat,aes(x=response, y=fs_indels,color=response))+geom_boxplot(outlier.shape = NA) +geom_quasirandom(size = 3, shape=18)+ theme(legend.position = "none",text = element_text(size=10),axis.text.x = element_text(size=10),axis.text.y = element_text(size=10))+stat_compare_means(comparisons = my_comparisons,)+scale_fill_manual(values=cbPalette2)+scale_color_manual(values = cbPalette2)+ theme(plot.margin = unit(c(1,1,1,1), "lines"))+xlab("")
 ac_nmd_ri<-ggplot(data=dat,aes(x=response, y=NMD_escape_fs,color=response)) +geom_boxplot(outlier.shape = NA)+geom_quasirandom(size = 3, shape=18)+ theme(legend.position = "none",text = element_text(size=10),axis.text.x = element_text(size=10),axis.text.y = element_text(size=10))+stat_compare_means(comparisons = my_comparisons,)+scale_fill_manual(values=cbPalette2)+scale_color_manual(values = cbPalette2)+ theme(plot.margin = unit(c(1,1,1,1), "lines"))+xlab("")+scale_y_continuous(limits=c(0,3))
 plot_grid(ac_snv_ri,ac_fs_ri,ac_nmd_ri,nrow=1)
+
+# MVA
+dat<-read.table("./combined_CPI_dat_all_final.txt",header=T)
+summary(glm(benefit~TMB+nmd_escape+study,data=dat,family=binomial(link='logit')))
 
 
