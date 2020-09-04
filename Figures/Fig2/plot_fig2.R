08dfeb08fe705097b8140053db4046ddcaf139df
kevlitchfield1 <40664105+kevlitchfield1@users.noreply.github.com>
2020-09-04 11:23:12 +0100

    Update plot_fig2.R

--- Figures/Fig2/plot_fig2.R
+++ Figures/Fig2/plot_fig2.R
@@ -12,8 +12,13 @@ library(scales)
 library('dplyr')
 library(ggbeeswarm)
 
+# panel B
+dat4<-read.table("./VAF_by_exon_dat.txt",header=T,sep='\t')
+dat4$exon_group<-factor(dat4$exon_group,levels = c("first","middle","penultimate_50bp_plus","penultimate_within_50bp","last"),ordered = TRUE)
+ggplot(data=dat4,aes(x=exon_group, y=VAF)) + geom_boxplot(aes(fill = exon_group),outlier.shape = NA)+ theme(legend.position = "none",text = element_text(size=10),axis.text.x = element_text(size=10),axis.text.y = element_text(size=10))+stat_compare_means()+ theme(plot.margin = unit(c(1,1,1,1), "lines"))+xlab("")+geom_quasirandom(color="grey",size=0.5)+ scale_fill_brewer(palette="Blues")
 
 
+# panel C
 cbPalette2<-c("deepskyblue","deepskyblue4")
 dat3<-read.table("./protein_expression_to_plot.txt",header=T)
 dat3$type<-factor(dat3$type,levels = c("fs_indel_not_expressed","fs_indel_expressed"),ordered = TRUE)
